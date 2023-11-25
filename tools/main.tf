8964de22895bd2c9bf58a62641489970332b489e
Christian Mahardhika <35090186+christianmahardhika@users.noreply.github.com>
2023-11-25 22:57:37 +0700

    add terraform manifest tools

---
+++ tools/main.tf
@@ -0,0 +1,102 @@
+provider "aws" {
+  region = "ap-southeast-1" # Singapore region
+}
+
+resource "tls_private_key" "terrafrom_generated_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name = "aws_keys_pairs"
+  # Public Key: The public will be generated using the reference of tls_private_key.terrafrom_generated_private_key
+  public_key = tls_private_key.terrafrom_generated_private_key.public_key_openssh
+
+  # Store private key :  Generate and save private key(aws_keys_pairs.pem) in current directory
+  provisioner "local-exec" {
+    command = <<-EOT
+       echo '${tls_private_key.terrafrom_generated_private_key.private_key_pem}' > aws_keys_pairs.pem
+       chmod 400 aws_keys_pairs.pem
+     EOT
+  }
+}
+
+resource "aws_instance" "tools" {
+  ami           = "ami-078c1149d8ad719a7" # Ubuntu 22.04 LTS
+  instance_type = "t2.small"
+  key_name      = "aws_keys_pairs"
+
+  vpc_security_group_ids = [
+    aws_security_group.tools.id
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("aws_keys_pairs.pem")
+    host        = self.public_ip
+    timeout     = "4m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install ca-certificates curl gnupg lsb-release -y",
+      "sudo install -m 0755 -d /etc/apt/keyrings",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+      "sudo chmod a+r /etc/apt/keyrings/docker.gpg",
+      "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y"
+    ]
+  }
+
+  tags = {
+    Name  = "tools-instance-cluster-fww"
+    Group = "prodigybe"
+  }
+}
+
+resource "aws_security_group" "tools" {
+  name_prefix = "tools-sg-"
+  description = "tools security group"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 30080
+    to_port     = 30080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Group" = "prodigybe"
+  }
+}
