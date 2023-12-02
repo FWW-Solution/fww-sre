6499fb4402e68fc5f2cad9bd0112668e8b068ee8
Christian Mahardhika <35090186+christianmahardhika@users.noreply.github.com>
2023-12-02 08:24:38 +0700

    fix seed data

--- deploy/local/database/seeder/seeder.sql
+++ deploy/local/database/seeder/seeder.sql
@@ -1,57 +1,17 @@
-INSERT INTO users (
-        full_name,
-        username,
-        email,
-        password,
-        created_at,
-        updated_at,
-        deleted_at
-    )
-VALUES (
-        'John Doe',
-        'johndoe',
-        'johndoe@example.com',
-        'password123',
-        NOW(),
-        NULL,
-        NULL
-    ),
-    (
-        'Jane Doe',
-        'janedoe',
-        'christian.mahardhika@gmail.com',
-        'password456',
-        NOW(),
-        NULL,
-        NULL
-    ),
-    (
-        'Bob Smith',
-        'bobsmith',
-        'bobsmith@example.com',
-        'password789',
-        NOW(),
-        NULL,
-        NULL
-    ),
-    (
-        'Alice Johnson',
-        'alicejohnson',
-        'alicejohnson@example.com',
-        'passwordabc',
-        NOW(),
-        NULL,
-        NULL
-    ),
-    (
-        'Mike Brown',
-        'mikebrown',
-        'mikebrown@example.com',
-        'passworddef',
-        NOW(),
-        NULL,
-        NULL
-    );
+
+INSERT INTO users (full_name, username, email, password, created_at, updated_at, deleted_at)
+VALUES 
+--password123
+('John Doe', 'johndoe', 'johndoe@example.com', 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f', NOW(), NULL, NULL),
+--password456
+('Jane Doe', 'janedoe', 'christian.mahardhika@gmail.com', 'c6ba91b90d922e159893f46c387e5dc1b3dc5c101a5a4522f03b987177a24a91', NOW(), NULL, NULL), 
+--password789
+('Bob Smith', 'bobsmith', 'bobsmith@example.com', '5efc2b017da4f7736d192a74dde5891369e0685d4d38f2a455b6fcdab282df9c', NOW(), NULL, NULL),
+--passwordabc
+('Alice Johnson', 'alicejohnson', 'alicejohnson@example.com', '6733b7ffeace4887c3b31258079c780d8db3018db9cbc05c500df3521f968df8', NOW(), NULL, NULL),
+--passworddef
+('Mike Brown', 'mikebrown', 'mikebrown@example.com', '478a7da128a2875a1484798da2010d8f518ab4f341000da93c59fc5c201ded2c', NOW(), NULL, NULL);
+
 INSERT INTO passengers (
         full_name,
         gender,
